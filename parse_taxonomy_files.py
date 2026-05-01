0443de411051c077e737b10729fcd628a501d15f
roman <porublyovyy952@gmail.com>
2026-05-01 15:30:43 -0500

    handle special cases in parsing of  xsd files

--- parse_taxonomy_files.py
+++ parse_taxonomy_files.py
@@ -74,8 +74,6 @@ def extract_calcs(xml_file):
 
     rows = []
 
-    if not calc_links:
-        print(xml_file)
     for calc_link in calc_links:
         link_type = calc_link.get(xlink_type)
         link_role = calc_link.get(xlink_role)
@@ -94,6 +92,72 @@ def extract_calcs(xml_file):
                 float(arc.get('order', 0)),
                 float(arc.get('weight', 0)),
             ))
+    
+
+    return rows
+
+
+def extract_calcs_special_case(xml_file):
+    name_parts = xml_file.stem.split('_')
+    cik              = name_parts[0]
+    accession_number = name_parts[1]
+    file_name        = "_".join(xml_file.name.split("_")[2:])
+
+    tree = etree.parse(xml_file)
+    root = tree.getroot()
+
+    # Dynamically resolve namespace URIs from root nsmap
+    ns = root.nsmap
+    linkbase_uri = ns.get('link', 'http://www.xbrl.org/2003/linkbase')
+    xlink_uri    = ns.get('xlink', 'http://www.w3.org/1999/xlink')
+
+    calc_link_tag = f"{{{linkbase_uri}}}calculationLink"
+    calc_arc_tag  = f"{{{linkbase_uri}}}calculationArc"
+    calc_loc_tag  = f"{{{linkbase_uri}}}loc"
+    xlink_type    = f"{{{xlink_uri}}}type"
+    xlink_role    = f"{{{xlink_uri}}}role"
+    xlink_arcrole = f"{{{xlink_uri}}}arcrole"
+    xlink_label   = f"{{{xlink_uri}}}label"
+    xlink_href    = f"{{{xlink_uri}}}href"
+
+    if root.tag == calc_link_tag:
+        calc_links = [root]
+    else:
+        calc_links = root.findall(f".//{calc_link_tag}")
+
+    rows = []
+
+
+    for calc_link in calc_links:
+        fromLink = None
+        toLink = None
+        link_type = calc_link.get(xlink_type)
+        link_role = calc_link.get(xlink_role)
+        for arc in calc_link.findall(calc_arc_tag):
+            if len(calc_link.findall(calc_loc_tag))==2:
+                if calc_link.findall(calc_loc_tag)[0].get(xlink_label) == 'src':
+                    fromLink = calc_link.findall(calc_loc_tag)[0].get(xlink_href).split('#')[-1]
+                    toLink = calc_link.findall(calc_loc_tag)[1].get(xlink_href).split('#')[-1]
+                elif calc_link.findall(calc_loc_tag)[0].get(xlink_label) == 'dest':
+                    toLink = calc_link.findall(calc_loc_tag)[0].get(xlink_href).split('#')[-1]
+                    fromLink = calc_link.findall(calc_loc_tag)[1].get(xlink_href).split('#')[-1]
+
+            rows.append((
+                cik,
+                accession_number,
+                file_name,
+                link_type,
+                link_role,
+                arc.get(xlink_type),
+                arc.get(xlink_arcrole),
+                fromLink,
+                toLink,
+                arc.get('use'),
+                float(arc.get('order', 0)),
+                float(arc.get('weight', 0)),
+            ))
+    
+
     return rows
 
 
@@ -141,4 +205,9 @@ if __name__ == "__main__":
     conn = ddb.connect(db_path)
     xml_taxonomy_dir = DATA_DIR / "calXMLs"
     write_calcs_to_db(conn, get_xml_taxonomy_files(conn, xml_taxonomy_dir), extract_calcs)
+
+    conn.execute('''delete from calculationTaxonomyRaw where arcXlinkFrom = 'src' or arcXlinkto = 'dest';
+                checkpoint;''')
+
+    write_calcs_to_db(conn, get_xml_taxonomy_files(conn, xml_taxonomy_dir), extract_calcs_special_case)
     conn.close()
